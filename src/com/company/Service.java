a10324c607fdda097ad18ee8f80a091c6cc54409
Patryk <patrykkorczak7@gmail.com>
2020-10-27 20:14:44 +0100

    almost completed server, need to clean up values displayed in console

---
+++ src/com/company/Service.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Service extends Thread {
+    Socket client;
+    boolean started = true;
+    byte [] message = new byte[1024];
+
+    public Service(Socket client) {
+        this.client = client;
+    }
+
+    public void run() {
+        System.out.println(this.client.getInetAddress().getHostName() + " connected from port " + this.client.getPort());
+
+        try {
+            DataInputStream toReceive = new DataInputStream(this.client.getInputStream());
+            DataOutputStream toSend = new DataOutputStream(this.client.getOutputStream());
+            while(this.started) {
+                int bytesReceived = toReceive.read(message);
+                String messageString = new String(this.message, 0, bytesReceived);
+                System.out.println(this.client.getInetAddress().getHostName() + ": " + messageString);
+                if(messageString.contains("closeConnection")) {
+                    this.started = false;
+                    System.out.println("Disconnecting " + this.client.getInetAddress().getHostName());
+                }
+
+                toSend.write(this.message, 0, bytesReceived);
+                toSend.flush();
+            }
+        } catch(Exception e) {
+            System.out.println(this.client.getInetAddress().getHostName() + " disconnected. " + e.getMessage());
+        }
+
+        try {
+            this.client.close();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
